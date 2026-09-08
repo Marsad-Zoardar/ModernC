a3eedcde9b7fb0385fcccb2c63f275a7d1f04077
Marsad-Zoardar <marsadzoardar@gmail.com>
2026-09-08 02:26:50 +0000

    Fixed bug in program 11

--- PointerWalking/11_LongestWordAndLength.c
+++ PointerWalking/11_LongestWordAndLength.c
@@ -12,7 +12,17 @@ const char *longestWordStartAndLength(const char *ptr);
 
 int main(void){
     
-    char string[] = "Today is programming day";
+    char string[] = "";
+/*"Today is programming day"
+"programming"
+""
+"     "
+"hi"
+"hi hello"
+"hello hi"
+"hello     hi"
+"   hello hi   "
+*/
     
     printf("The longest word starts at: %p\n\n", longestWordStartAndLength(string));
     
@@ -26,6 +36,7 @@ const char *longestWordStartAndLength(const char *ptr){
     size_t currentLength = 0;
     size_t count = 0;
     
+    
     //printf("Entering while loop...\n\n");
     while(*ptr != 0){
         
@@ -34,7 +45,7 @@ const char *longestWordStartAndLength(const char *ptr){
             //printf("Current pointer address = %p\n", ptr);
             ptr++;
         }else{
-            //printf("%c", *ptr);
+            printf("%c", *ptr);
             //printf("Found a character. Counting...\n");
             count++;
             //printf("Current pointer address = %p\n", ptr);
@@ -70,16 +81,20 @@ const char *longestWordStartAndLength(const char *ptr){
                    longestWordStart = ptr - currentLength;
                }
     }
-    printf("Max Length = %zu\n", maxLength);
-    //printf("longestWordStart = %p\n\n", (void*)longestWordStart);
-    printf("First char of the longest word = %c\n", *longestWordStart);
-    printf("The longest word = ");
+    if(maxLength > 0){
+        printf("Max Length = %zu\n", maxLength);
+        //printf("longestWordStart = %p\n\n", (void*)longestWordStart);
+        printf("First char of the longest word = %c\n", *longestWordStart);
+        printf("The longest word = ");
     
-//    printf("Longest word start = %s", longestWordStart);
-    for(int i = 0; i < maxLength; i++){
-        printf("%c", *(longestWordStart + i));
+//      printf("Longest word start = %s", longestWordStart);
+        for(size_t i = 0; i < maxLength; i++){
+            printf("%c", *(longestWordStart + i));
+        }
+        printf("\n\n");
+    }else{
+        printf("The string is empty!\n\n");
     }
-    printf("\n\n");
     
     return longestWordStart;
 }
