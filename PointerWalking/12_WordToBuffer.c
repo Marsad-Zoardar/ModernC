37713fe928e34d8ddc2dc21836a75f973b96bda4
Marsad-Zoardar <marsadzoardar@gmail.com>
2026-09-08 06:20:36 +0000

    Problem 12 done

--- PointerWalking/12_WordToBuffer.c
+++ PointerWalking/12_WordToBuffer.c
@@ -11,6 +11,13 @@
 void wordToBuffer(const char *src, char *dest, size_t destSize);
 
 int main(void){
+    
+    size_t destSize = 50;
+    char src[] = "  ";
+    char dest[destSize];
+    
+    wordToBuffer(src, dest, destSize); 
+    
     return 0;
 }
 
@@ -29,22 +36,60 @@ void wordToBuffer(const char *src, char *dest, size_t destSize){
     size_t count = 0;//This is to count the current word
     
     //This while loop reads through the string
+    printf("Entering while loop......\n\n");
+    printf("--------------------------------------\n\n");
     while(*src != '\0'){
         if(isspace((unsigned char)*src)){
             //Skips the space by incrementing the pointer
+            printf("Space detected! Skipping >>>>>\n");
             src++;
         }else{
+            printf("Word found! Counting\n");
             count++;
             src++;
             if(isspace((unsigned char)*src)){
+                printf("Word ends. Length of the word: %zu\n", count);
                 currentLength = count;
                 if(currentLength > maxLength){
+                    printf("Current length is longer than max length. Setting max length to current length\n\n");
                     maxLength = currentLength;
-                    longestWordStart = src;
+                    printf("Current address is: %p----->\n", src);
+                    longestWordStart = src - currentLength;
+                    printf("Current longest word starting at: %p------>\n", longestWordStart);
+                    printf("Current value of the *longestWordStart = %c\n", *longestWordStart);
                 }
+                printf("Resetting count to 0!\n\n");
                 count = 0;
             }
         }
     }
+    printf("While loop ends!\n");
+    printf("------------------------------\n\n");
+    if(count != 0){
+        printf("The string ended properly. Count is greater than 1. Checking for new max length\n\n");
+        currentLength = count;
+                if(currentLength > maxLength){
+                    printf("Current length is longer than max length. Setting max length to current length\n\n");
+                    maxLength = currentLength;
+                    printf("Current address is: %p----->\n", src);
+                    longestWordStart = src - currentLength;
+                    printf("Current longest word starting at: %p------>\n", longestWordStart);
+                    printf("Current value of the *longestWordStart = %c\n", *longestWordStart);
+                }
+    }
+    printf("Checking if max length is greater than 0 or not.....\n\n");
+    if(maxLength > 0){
+        printf("Current value of the *longestWordStart = %c\n", *longestWordStart);
+        for(size_t i = 0; i < maxLength; i++){
+            //printf("dest[i] = %c\t *longestWordStart = %c\n", dest[i], *longestWordStart);
+            dest[i] = *(longestWordStart + i);
+            printf("dest[i] = %c\t *longestWordStart = %c\n", dest[i], *(longestWordStart + i));
+        }
+        dest[maxLength] = '\0';
+        printf("The longest word is: %s\n", dest);
+    }else{
+        printf("The string is empty!\n\n");
+    }
+    
     
 }
